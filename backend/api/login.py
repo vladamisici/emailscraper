2dca3be11e3bcaf919b5f45fcd8ce71ccbe04d98
andydobre0 <dobreandy1@gmail.com>
2024-05-07 18:22:25 +0300

    endpoints for oauth with google and gmail api for retrieving the inbox

--- backend/api/login.py
+++ backend/api/login.py
@@ -1,12 +1,19 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for, session
 import jwt
 from models.user import User
+from werkzeug.security import check_password_hash
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import Flow
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+CLIENT_ID = '339144602283-s2b47qd4vh5hk8ksu68as6b2lp8v1g5d.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+CLIENT_SECRET_FILE = "client_secret_email.json"
+AUTH_URL = 'http://localhost'
+
+SCOPES = ["https://mail.google.com/"]
 
 login_bp = Blueprint('login', __name__, url_prefix='/login')
-
-# @login_bp.route('/login')
-# def login():
-#     return jsonify({"message":"logged in"})
 
 @login_bp.route('/login', methods=['POST'])
 def login():
@@ -16,11 +23,46 @@ def login():
 
     user = User.get_user_by_username(username=username)
 
-
+    print(User.hash_password(password))
     if user:
-        if(User.check_password(user,password)):
+        if(user.check_password(password)):
             return jsonify({'message':'login succesful'}),200
         else:
             return jsonify({'error':'password incorrect'}),401
     else:
         return jsonify({'login error':'user not found'}),404
+
+@login_bp.route('/login_oauth', methods=['GET'])
+def login_oauth():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRET_FILE, scopes=SCOPES
+    )
+    flow.redirect_uri = url_for('.callback', _external=True)
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        prompt='select_account'
+    )
+    session['state'] = state
+    return redirect(authorization_url)
+
+@login_bp.route('/callback')
+def callback():
+    if request.args.get('state') != session.pop('state', None):
+        return 'Invalid state', 400
+    flow = InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRET_FILE, scopes=SCOPES
+    )
+    flow.redirect_uri = url_for('.callback', _external=True)
+
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    credentials = flow.credentials
+    session['credentials'] = credentials.to_json()
+
+    return redirect(url_for('index'))
+
+@login_bp.route('/logout')
+def logout():
+    session.pop('credentials', None)
+    return redirect(url_for('index'))
---
+++ backend/api/scrape.py
@@ -0,0 +1,127 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from flask import Flask, jsonify, Blueprint, session
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://mail.google.com/"]
+
+emails_bp = Blueprint('emails', __name__, url_prefix='/emails')
+
+@emails_bp.route("/emails")
+def get_emails():
+    credentials_json = session.get('credentials')
+    if credentials_json:
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                "../client_secret_email.json", SCOPES
+        )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+        try:
+            # Call the Gmail API
+            service = build("gmail", "v1", credentials=creds)
+            results = service.users().messages().list(userId="me", labelIds=["INBOX"]).execute()    
+            emails = results.get("messages", [])
+
+            if not emails:
+                return "No emails", 200
+    
+            inbox_content = []
+            for email in emails:
+                msg = service.users().messages().get(userId="me", id=email["id"]).execute()
+                subject = next((header['value'] for header in msg['payload']['headers'] if header['name'] == 'Subject'), None)
+                snippet = msg.get('snippet', '')
+                inbox_content.append({'subject':subject, 'snippet':snippet})
+            return jsonify(inbox_content), 200
+    
+        except Exception as error:
+            return f'Error: {str(error)}', 500
+    else:
+        return 'Unauthorized', 401
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import imaplib
+# import email
+# import yaml
+# import jsonify
+
+# from flask import Blueprint
+
+# with open("credentials.yaml") as f:
+#     content = f.read()
+
+# credentials = yaml.load(content, Loader=yaml.FullLoader)
+
+# user, password = credentials['user'], credentials['password']
+
+
+
+
+
+
+# def fetch_emails(user, password):
+#     imap_url = "imap.gmail.com"
+#     my_mail = imaplib.IMAP4_SSL(imap_url)
+#     my_mail.login(user,password)
+#     my_mail.select("INBOX")
+#     _, data = my_mail.search(None, 'ALL')
+#     id_list = data[0].split()
+#     messages = []
+
+#     for msg in id_list:
+#         typ, data = my_mail.fetch(msg, '(RFC822)')
+#         messages.append(data)
+
+#     emails = []
+
+#     for msg in messages[::-1]:
+#         for response in msg:
+#             if isinstance(response, tuple):
+#                 my_msg = email.message_from_bytes((response[1]))
+#                 email_data = {
+#                     'subject': my_msg['subject'],
+#                     'from': my_msg['from'],
+#                     'body': ''
+#                 }
+#                 for part in my_msg.walk():
+#                     if part.get_content_type() == 'text/plain':
+#                         email_data['body'] = part.get_payload()
+#                 emails.append(email_data)
+#     return emails
+
