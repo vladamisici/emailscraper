ef4d5837219231f39023f5b7439180a8ac5c64c3
andydobre0 <dobreandy1@gmail.com>
2024-05-08 02:02:27 +0300

    minor fixes

--- backend/api/scrape.py
+++ backend/api/scrape.py
@@ -3,7 +3,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from flask import Flask, jsonify, Blueprint, session
+from flask import jsonify, Blueprint, session, redirect, url_for
 from dotenv import load_dotenv
 import os
 import json
@@ -12,10 +12,10 @@ load_dotenv()
 SCOPES = ["https://mail.google.com/"]
 CLIENT_SECRET_FILE = os.getenv('CLIENT_SECRET_FILE')
 
-emails_bp = Blueprint('emails', __name__, url_prefix='/emails')
+emails_bp = Blueprint('emails', __name__)
 
-@emails_bp.route("/emails")
-def get_emails():
+@emails_bp.route("/inbox")
+def get_inbox():
     credentials_json = session.get('credentials')
 
     Credentials_info = json.loads(credentials_json)
@@ -47,83 +47,17 @@ def get_emails():
                 msg = service.users().messages().get(userId="me", id=email["id"]).execute()
                 subject = next((header['value'] for header in msg['payload']['headers'] if header['name'] == 'Subject'), None)
                 snippet = msg.get('snippet', '')
-                inbox_content.append({'subject': subject, 'snippet': snippet})
+                sender = next((header['value'] for header in msg['payload']['headers'] if header['name'] == 'From'), None)
+                timestamp = msg.get('internalDate', '')
+                inbox_content.append({'subject': subject, 'snippet': snippet, 'from': sender, 'time': timestamp})
             return jsonify(inbox_content), 200
     
         except Exception as error:
             return f'Error: {str(error)}', 500
     else:
         return 'Unauthorized', 401
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import imaplib
-# import email
-# import yaml
-# import jsonify
-
-# from flask import Blueprint
-
-# with open("credentials.yaml") as f:
-#     content = f.read()
-
-# credentials = yaml.load(content, Loader=yaml.FullLoader)
-
-# user, password = credentials['user'], credentials['password']
-
-
-
-
-
-
-# def fetch_emails(user, password):
-#     imap_url = "imap.gmail.com"
-#     my_mail = imaplib.IMAP4_SSL(imap_url)
-#     my_mail.login(user,password)
-#     my_mail.select("INBOX")
-#     _, data = my_mail.search(None, 'ALL')
-#     id_list = data[0].split()
-#     messages = []
-
-#     for msg in id_list:
-#         typ, data = my_mail.fetch(msg, '(RFC822)')
-#         messages.append(data)
-
-#     emails = []
-
-#     for msg in messages[::-1]:
-#         for response in msg:
-#             if isinstance(response, tuple):
-#                 my_msg = email.message_from_bytes((response[1]))
-#                 email_data = {
-#                     'subject': my_msg['subject'],
-#                     'from': my_msg['from'],
-#                     'body': ''
-#                 }
-#                 for part in my_msg.walk():
-#                     if part.get_content_type() == 'text/plain':
-#                         email_data['body'] = part.get_payload()
-#                 emails.append(email_data)
-#     return emails
-
+    
+@emails_bp.route('/logout')
+def logout():
+    session.pop('credentials', None)
+    return redirect(url_for('home'))
